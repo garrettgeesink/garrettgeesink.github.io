b531f9b6201ac646c3eff31b2ef40f1d92c9f43b
garrettgeesink <chatmansave@gmail.com>
2023-09-07 20:46:35 -0700

    Changed footer link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/garretgeesink/website'
+      href='https://garrettgeesink.github.io/'
       className='link footer__link'
     >
       Created By Garrett Geesink
